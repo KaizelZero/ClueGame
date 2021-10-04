a177941788248e8752e4bf3d6bb15046c7fe9d48
Jimmy Truong <54197644+jqt-designs@users.noreply.github.com>
2021-10-04 16:44:34 -0600

    Comments and formatting

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestBoard {
+	
+	// Variables
 	private Set<TestBoardCell> targets = new HashSet<TestBoardCell>();
 	private ArrayList<ArrayList<TestBoardCell>> board = new ArrayList<ArrayList<TestBoardCell>>();
 	public void calcTargets(TestBoardCell startCell, int pathlength) {}
 	
+	// Creates a TestBoard
 	public TestBoard(int width, int height) {
 		super();
 		for(int i = 0; i < width; i++) {
@@ -19,15 +22,13 @@ public class TestBoard {
 			board.add(temp);
 		}
 	}
-
-	public TestBoard() {
-
-	}
-
+	
+	// Returns targets ArrayList
 	public Set<TestBoardCell> getTargets(){
 		return targets;
 	}
 	
+	// Returns the instance of a call at a certain position
 	public TestBoardCell getCell(int row, int col) {
 		return board.get(col).get(row);
 	}
