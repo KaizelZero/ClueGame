186b96b4858d818045bc3844e7fa5a5fa25971d7
Tonythepigeon <tonythepigeonv.3@gmail.com>
2021-10-04 11:17:57 -0600

    Added TestBoardCell class with methods

---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,32 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoardCell {
+	private int row, column;
+	private Set<TestBoardCell> adjacencyList;
+	private boolean isRoom = false, isOccupied = false;
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	void addAdjacency(TestBoardCell cell) {
+		
+	}
+	Set<TestBoardCell> getAdjList(){
+		return this.adjacencyList;
+	}
+	void setRoom(boolean isAssignRoom) {
+		isRoom = isAssignRoom;
+	}
+	boolean getRoom() {
+		return isRoom;
+	}
+	void setOccupied(boolean newOccupied) {
+		isOccupied = newOccupied;
+	}
+	boolean getOccupied() {
+		return isOccupied;
+	}
+}
